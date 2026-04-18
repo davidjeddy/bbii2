fed63b4fd827257dd0f1e67ef7d83a51c6d1be01
David Eddy <me@davidjeddy.com>
2026-04-18 12:22:48 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/bbii2

---
+++ views/forum/_forum.php
@@ -0,0 +1,73 @@
+<?php
+
+use yii\helpers\Html;
+
+use frontend\modules\bbii\AppAsset;
+$assets = AppAsset::register($this);
+
+/* @var $this ForumController */
+/* @var $data BbiiForum */
+
+	$image = 'forum';
+	if(!isset($data->last_post_id) || $this->forumIsRead($data->id)) {
+		$image .= '2';
+	} else {
+		$image .= '1';
+	}
+	if($data->locked) {
+		$image .= 'l';
+	}
+	if($data->moderated) {
+		$image .= 'm';
+	}
+	if(!$data->public) {
+		$image .= 'h';
+	}
+?>
+
+<?php if($data->type): ?>
+<div class = "forum">
+	<div class = "forum-cell <?php echo $image; ?>"></div>
+	<div class = "forum-cell main">
+		<div class = "header2">
+			<?php echo Html::a(Html::encode($data->name), array('forum', 'id' => $data->id)); ?>
+		</div>
+		<div class = "header4">
+			<?php echo Html::encode($data->subtitle); ?>
+		</div>
+	</div>
+	<div class = "forum-cell center">
+		<?php echo Html::encode($data->num_posts); ?><br>
+		<?php echo Html::encode($data->getAttributeLabel('num_posts')); ?>
+	</div>
+	<div class = "forum-cell center">
+		<?php echo Html::encode($data->num_topics); ?><br>
+		<?php echo Html::encode($data->getAttributeLabel('num_topics')); ?>
+	</div>
+	<div class = "forum-cell last-cell">
+		<?php if($data->last_post_id && $data->lastPost) {
+			echo Html::encode($data->lastPost->poster->member_name);
+			echo Html::a(Html::img($assets->baseUrl.'/images/next.png'), 'next', array('style' => 'margin-left:5px;')), array('topic', 'id' => $data->lastPost->topic_id, 'nav' => 'last'));
+			echo '<br>';
+			echo DateTimeCalculation::long($data->lastPost->create_time);
+		} else {
+			echo Yii::t('BbiiModule.bbii', 'No posts');
+		}
+		?>
+	</div>
+</div>
+
+<?php else: ?>
+	<?php if($index > 0) { echo '</div>'; } ?>
+	<div class = "forum-category" onclick = "BBii.toggleForumGroup(<?php echo $data->id; ?>,'<?php echo Yii::$app->createAbsoluteUrl($this->module->id.'/forum/setCollapsed'); ?>');">
+		<div class = "header2">
+			<?php echo Html::encode($data->name); ?>
+		</div>
+		<div class = "header4">
+			<?php echo Html::encode($data->subtitle); ?>
+		</div>
+	</div>
+	<div class = "forum-group" id = "category_<?php echo $data->id; ?>" <?php if($this->collapsed($data->id)) { echo 'style = "display:none;"';}?>>
+<?php endif; ?>
+
+<?php if($index == $lastIndex) { echo '</div>'; } ?>
---
+++ views/member/_header.php
@@ -0,0 +1,44 @@
+<?php
+
+use frontend\modules\bbii\models\BbiiMessage;
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\widgets\Breadcrumbs;
+
+use frontend\modules\bbii\AppAsset;
+$assets = AppAsset::register($this);
+
+/* @var $this ForumController */
+/* @var $item array */
+/* @var $breadcrumbs array */
+?>
+<div id = "bbii-header">
+	<?php if(!Yii::$app->user->isGuest): ?>
+	<?php $messages = BbiiMessage::find()->inbox()->unread()->count('sendto = '.Yii::$app->user->id); ?>
+		<div class = "bbii-profile-box">
+		<?php 
+			if($messages) {
+				echo Html::a(Html::img($assets->baseUrl.'/images/newmail.png', array('title' => $messages . ' ' . Yii::t('BbiiModule.bbii', 'new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox')); 
+			} else {
+				echo Html::a(Html::img($assets->baseUrl.'/images/mail.png', array('title' => Yii::t('BbiiModule.bbii', 'no new messages'), 'style' => 'vertical-align:bottom;')), array('message/inbox')); 
+			}
+			echo ' | '; echo Html::a(Html::img($assets->baseUrl.'/images/settings.png', array('title' => Yii::t('BbiiModule.bbii', 'My settings'), 'style' => 'vertical-align:bottom;')), array('member/view', 'id'  => Yii::$app->user->id)); 
+		?>
+		</div>
+	<?php endif; ?>
+	<div class = "bbii-title"><?php echo $this->context->module->forumTitle; ?></div>
+	<table style = "margin:0;"><tr><td style = "padding:0;">
+		<div id = "bbii-menu">
+		<?php  echo Nav::widget([
+		    'items' => $item,
+		]); ?>
+		</div>
+	</td></tr></table>
+</div>
+<?php if(isset($this->context->bbii_breadcrumbs)):?>
+	<?php echo Breadcrumbs::widget(array(
+		'homeLink' => false,
+		'links' => $this->context->bbii_breadcrumbs,
+	)); ?><!-- breadcrumbs -->
+<?php endif?>
