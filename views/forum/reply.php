2fcf2e1da980b358676ed05ec6f26e109ee6bcb7
David Eddy <me@davidjeddy.com>
2026-04-18 12:22:42 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/bbii2

---
+++ views/forum/reply.php
@@ -0,0 +1,27 @@
+<?php
+/* @var $this ForumController */
+/* @var $forum BbiiForum */
+/* @var $topic BbiiTopic */
+/* @var $post BbiiPost */
+
+// @todo disabled for initial release - DJE : 2015-05-28
+/*$this->context->bbii_breadcrumbs = array(
+	Yii::t('BbiiModule.bbii', 'Forum') => array('forum/index'),
+	$forum->name => array('forum/forum', 'id' => $forum->id),
+	$topic->title => array('forum/topic', 'id' => $topic->id),
+	Yii::t('BbiiModule.bbii', 'Reply'),
+);*/
+
+$this->title = Yii::t('forum', $topic->title);
+$this->params['breadcrumbs'][] = $this->title;
+
+$item = array(
+	array('label' => Yii::t('BbiiModule.bbii', 'Forum'), 	'url' => array('forum/index')),
+	array('label' => Yii::t('BbiiModule.bbii', 'Members'), 	'url' => array('member/index'))
+);
+?>
+<div id = "bbii-wrapper">
+	<?php echo $this->render('_header', array('item' => $item)); ?>
+	
+	<?php echo $this->render('_form', array('post' => $post)); ?>
+</div>
---
+++ views/moderator/report.php
@@ -0,0 +1,122 @@
+﻿<?php
+
+use frontend\modules\bbii\models\BbiiPost;
+use frontend\modules\bbii\models\BbiiMessage;
+
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+use frontend\modules\bbii\AppAsset;
+$assets = AppAsset::register($this);
+
+/* @var $this ModeratorController */
+/* @var $model BbiiMessage */
+
+$this->title = Yii::t('forum', 'Forum');
+$this->params['breadcrumbs'][] = $this->title;
+
+$this->context->bbii_breadcrumbs = array(
+	Yii::t('BbiiModule.bbii', 'Forum') => array('forum/index'),
+	Yii::t('BbiiModule.bbii', 'Reports'),
+);
+
+$approvals = BbiiPost::find()->unapproved()->count();
+$reports = BbiiMessage::find()->report()->count();
+
+$item = array(
+	array('label' => Yii::t('BbiiModule.bbii', 'Forum'), 							'url' => array('forum/index')),
+	array('label' => Yii::t('BbiiModule.bbii', 'Members'), 							'url' => array('member/index')),
+	array('label' => Yii::t('BbiiModule.bbii', 'Approval'). ' (' . $approvals . ')','url' => array('moderator/approval'), 	'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Reports'). 	' (' . $reports . ')', 	'url' => array('moderator/report'), 	'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Posts'), 							'url' => array('moderator/admin'), 		'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Blocked IP'), 						'url' => array('moderator/ipadmin'), 	'visible' => $this->context->isModerator()),
+	array('label' => Yii::t('BbiiModule.bbii', 'Send mail'), 						'url' => array('moderator/sendmail'), 	'visible' => $this->context->isModerator()),
+);
+?>
+
+<div id = "bbii-wrapper">
+	<?php echo $this->render('_header', array('item' => $item)); ?>
+
+	<?php // @depricated 2.2 Kept for referance
+	/*$this->widget('zii.widgets.grid.CGridView', array(
+		'id' => 'message-grid',
+		'dataProvider' => $model->search(),
+		'filter' => $model,
+		'columns' => array(
+			array(
+				'name' => 'sendfrom',
+				'value' => '$data->sender->member_name',
+				'filter' => false,
+			),
+			'subject',
+			array(
+				'name' => 'content',
+				'type' => 'html',
+			),
+			'create_time',
+			array(
+				'class' => 'yii\grid\ActionColumn',
+				'template' => '{view}{reply}{delete}',
+				'buttons' => array(
+					'view' => array(
+						'url' => 'array("forum/topic", "id" => $data->forumPost->topic_id, "nav" => $data->post_id)',
+						'label' => Yii::t('BbiiModule.bbii','Go to post'),
+						'imageUrl' => $assets->baseUrl.'/images/goto.png',
+					),
+					'reply' => array(
+						'url' => 'array("message/reply", "id" => $data->id)',
+						'label' => Yii::t('BbiiModule.bbii','Reply'),
+						'imageUrl' => $assets->baseUrl.'reply.png',
+						'options' => array('style' => 'margin-left:5px;'),
+					),
+					'delete' => array(
+						'url' => 'array("message/delete", "id" => $data->id)',
+						'imageUrl' => $assets->baseUrl.'/images/delete.png',
+						'options' => array('style' => 'margin-left:5px;'),
+					),
+				)
+			),
+		),
+	));*/ ?>
+
+	<?php
+	echo GridView::widget(array(
+		'columns'      => array(
+			array(
+				'filter' => false,
+				'header' => 'Send From',
+				'value'  => '$data->sender->member_name',
+			),
+			'subject',
+			array(
+				'header' => 'Content',
+				'format'   => 'html',
+			),
+			'create_time',
+			array(
+				'class'    => 'yii\grid\ActionColumn',
+				'template' => '{view}{reply}{delete}',
+				'buttons'  => array(
+					'view' => array(
+						'url' => 'array("forum/topic", "id" => $data->forumPost->topic_id, "nav" => $data->post_id)',
+						'label' => Yii::t('BbiiModule.bbii','Go to post'),
+						'imageUrl' => $assets->baseUrl.'/images/goto.png',
+					),
+					'reply' => array(
+						'url' => 'array("message/reply", "id" => $data->id)',
+						'label' => Yii::t('BbiiModule.bbii','Reply'),
+						'imageUrl' => $assets->baseUrl.'reply.png',
+						'options' => array('style' => 'margin-left:5px;'),
+					),
+					'delete' => array(
+						'url' => 'array("message/delete", "id" => $data->id)',
+						'imageUrl' => $assets->baseUrl.'/images/delete.png',
+						'options' => array('style' => 'margin-left:5px;'),
+					),
+				)
+			),
+		),
+		'dataProvider' => $model->search(),
+		'id'           => 'message-grid',
+	)); ?>
+</div>
