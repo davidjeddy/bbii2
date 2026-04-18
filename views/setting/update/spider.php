8877c28a064f0c83e958e9f41c92303d775e215d
David Eddy <me@davidjeddy.com>
2026-04-18 12:22:17 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/bbii2

---
+++ views/setting/update/spider.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+/* @var $this SettingController */
+/* @var $model BbiiMembergroup */
+/* @var $form ActiveForm */
+?>
+
+<?php echo $this->render('../template/_header'); ?>
+
+<br />
+
+<div class = "form">
+
+	<?php $form = ActiveForm::begin([
+		'enableAjaxValidation' => false,
+		'id'                   => 'edit-spider-form',
+	]); ?>
+
+		<?php echo $form->errorSummary($model); ?>
+
+		<div>
+			<?php echo $form->field($model,'name')->textInput(array('size' => 25)); ?>
+		</div>
+
+		<div>
+			<?php echo $form->field($model,'user_agent')->textInput(array('size' => 70)); ?>
+		</div>
+
+		<div>
+			<?php echo $form->field($model,'hits')->textInput(array('size' => 11)); ?>
+		</div>
+
+		<div>
+			<?php echo $form->field($model,'id')->hiddenInput()->label(false); ?>
+		</div>
+
+		<div class = "button">
+			<?php echo Html::submitButton(Yii::t('BbiiModule.bbii','Save'), array('class' => 'btn btn-success btn-lg')); ?>
+		</div>
+
+	<?php ActiveForm::end(); ?>
+
+</div><!-- form -->
